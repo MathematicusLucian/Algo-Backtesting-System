f0285748ba34e6c47db79ce512e95ea4adad5664
k1n4kut4 <recruitlukejones@gmail.com>
2024-03-25 16:02:29 +0000

    Created class CoinData

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-from livecoinwatch import LiveCoinWatch
 from dotenv import dotenv_values
+from src.coindata import CoinData
 
-config = dotenv_values(".env")
-api_key = config['LIVECOINWATCH_API_KEY']
-base_currency = "GBP"
-coins = ["ETH","BTC","XRP","SHIB"]
-coin_data = LiveCoinWatch(api_key, "GBP")
+def get_api_key():
+    config = dotenv_values(".env")
+    return config['LIVECOINWATCH_API_KEY']
 
-for coin in coins:
-    # Call 'coins/single/history'
-    historic_values = coin_data.coin__history(
-        code=coin,
-        currency=base_currency,
-        start=1617035100000,
-        end=1617035400000)
-    print(historic_values)
-    print("\n")
+if __name__ == '__main__':
 
-# Call 'coins/map'
-latest_values = coin_data.coins__map(
-            codes= coins,
-            currency=base_currency,
-            sort="rank",
-            offset=0,
-            limit=0)
-print(latest_values)
+    api_key = get_api_key()
+    base_currency = "GBP"
+    coins = ["ETH","BTC","XRP","SHIB"]
+    coin_data = CoinData(api_key, base_currency)
+
+    history = coin_data.historic_values(coins)
+    print(history)
+
+    latest = coin_data.latest_values(coins)
+    print(latest)
---
+++ src/coindata.py
@@ -0,0 +1,20 @@
+from livecoinwatch import LiveCoinWatch
+
+class CoinData:
+
+    def __init__(self, api_key, base_currency):
+        self.api_key = api_key
+        self.base_currency = base_currency
+        self.coin_data_api = LiveCoinWatch(api_key, base_currency)
+
+    # Call 'coins/single/history'
+    def historic_values(self, coins):
+        historic_values = []
+        for coin in coins:
+            coin_history = self.coin_data_api.coin__history(code=coin,currency=self.base_currency,start=1617035100000,end=1617035400000)
+            historic_values.append(coin_history)
+        return historic_values
+
+    # Call 'coins/map'
+    def latest_values(self, coins):
+        return self.coin_data_api.coins__map(codes=coins, currency=self.base_currency, sort="rank", offset=0, limit=0)
