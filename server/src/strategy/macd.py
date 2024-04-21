f14c679008817ec0ad5f0f2d198532f44a5f313b
k1n4kut4 <recruitlukejones@gmail.com>
2024-04-21 02:05:30 +0100

    MACD class

--- server/src/strategy/macd.py
+++ server/src/strategy/macd.py
@@ -1,85 +1,97 @@
 import yfinance as yf
-# import matplotlib.pyplot as plt
 import numpy as np
+from pandas import DataFrame
 import pandas_ta as ta
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-df = yf.Ticker('BTC-USD').history(period='1y')[map(str.title, ['open', 'close', 'low', 'high', 'volume'])]
+class MACD:
+    def __init__(self, fast=12, slow=26, signal=9):
+        self.df:DataFrame=self.get_currency_data('BTC-USD', '1y')
+        self.macd:DataFrame
+        self.fast=fast
+        self.slow=slow
+        self.signal=signal
+        self.macdh_config = str(f'macdh_{fast}_{slow}_{signal}')
+        self.macds_config = str(f'macds_{fast}_{slow}_{signal}')
+        self.calculate_MACD()
 
-df.ta.macd(close='close', fast=12, slow=26, append=True)
-df.columns = [x.lower() for x in df.columns]
+    def macd_data(self):
+        macd_data=dict()
+        macd_data["macd"]=self.macd
+        macd_data["price_line"]=self.price_line()
+        macd_data["fast_signal"]=self.fast_signal()
+        macd_data["slow_signal"]=self.slow_signal()
+        macd_data["histogram"]=self.get_histogram()
+        return macd_data
 
-fig = make_subplots(rows=2, cols=1)
+    def get_currency_data(self, pair, period):
+        return yf.Ticker(pair).history(period=period)[map(str.title, ['open', 'close', 'low', 'high', 'volume'])]
 
-fig.append_trace( #price line
-    go.Scatter(
-        x=df.index,
-        y=df['open'],
-        line=dict(color='#ff9900', width=1),
-        name='open',
-        # showlegend=False,
-        legendgroup='1',
-    ), row=1, col=1
-)
-
-fig.append_trace( # candlestick chart for pricing
-    go.Candlestick(
-        x=df.index,
-        open=df['open'],
-        high=df['high'],
-        low=df['low'],
-        close=df['close'],
-        increasing_line_color='#ff9900',
-        decreasing_line_color='black',
-        showlegend=False
-    ), row=1, col=1
-)
-# Fast Signal (%k)
-fig.append_trace(
-    go.Scatter(
-        x=df.index,
-        y=df['macd_12_26_9'],
-        line=dict(color='#ff9900', width=2),
-        name='macd',
-        # showlegend=False,
-        legendgroup='2',
-    ), row=2, col=1
-)
-
-fig.append_trace( # slow signal (%d)
-    go.Scatter(
-        x=df.index,
-        y=df['macds_12_26_9'],
-        line=dict(color='#000000', width=2),
-        # showlegend=False,
-        legendgroup='2',
-        name='signal'
-    ), row=2, col=1
-)
-
-historgram_colorized = np.where(df['macdh_12_26_9'] < 0, '#000', '#ff9900')
-
-fig.append_trace( # plot the histogram
-    go.Bar(
-        x=df.index,
-        y=df['macdh_12_26_9'],
-        name='histogram',
-        marker_color=historgram_colorized,
-    ), row=2, col=1
-)
-
-layout = go.Layout(
-    plot_bgcolor='#efefef',
-    font_family='Monospace',
-    font_color='#000000',
-    font_size=20,
-    xaxis=dict(
-        rangeslider=dict(
-            visible=False
+    def calculate_MACD(self):
+        self.macd = self.df.ta.macd(close='close', fast=12, slow=26, append=True)
+        self.macd.columns = [x.lower() for x in self.macd.columns]
+    
+    def get_historgram_colorized(self, df: DataFrame):
+        return np.where(df[self.macdh_config] < 0, '#000', '#ff9900')
+    
+    def get_layout():
+        return go.Layout(
+            plot_bgcolor='#efefef',
+            font_family='Monospace',
+            font_color='#000000',
+            font_size=20,
+            xaxis=dict(
+                rangeslider=dict(
+                    visible=False
+                )
+            )
         )
-    )
-)
+    
+    def get_scatter(self, name, color, y, width, legendgroup):
+        return go.Scatter(
+                x=self.macd.index,
+                y=y,
+                line=dict(color=color, width=width),
+                legendgroup=legendgroup,
+                name=name
+            )
+    
+    def get_candlesticks(self):
+        return go.Candlestick(
+                x=self.macd.index,
+                open=self.macd['open'],
+                high=self.macd['high'],
+                low=self.macd['low'],
+                close=self.macd['close'],
+                increasing_line_color='#ff9900',
+                decreasing_line_color='black',
+                showlegend=False
+            )
+    
+    def price_line(self):
+        return self.get_scatter('open', '#ff9900', self.macd['open'], 1, '1')
+    
+    def fast_signal(self):
+        return self.get_scatter('Fast Signal (k)', '#ff9900', self.macd[self.macds_config], 2, '2')
+    
+    def slow_signal(self):
+        return self.get_scatter('Slow signal (d)', '#000000', self.macd[self.macds_config], 2, '2')
+    
+    def get_histogram(self):
+        return go.Bar(
+                x=self.macd.index,
+                y=self.macd[self.macdh_config],
+                name='histogram',
+                marker_color=self.get_historgram_colorized(self.macd),
+            )
 
-fig.update_layout(layout)
-fig.show()
+    def create_fig(self):
+        fig = make_subplots(rows=2, cols=1)
+        fig.append_trace(self.price_line(), row=1, col=1)
+        fig.append_trace(self.get_candlesticks(), row=1, col=1)
+        fig.append_trace(self.fast_signal(), row=2, col=1)
+        fig.append_trace(self.slow_signal(), row=2, col=1)
+        fig.append_trace(self.get_histogram(), row=2, col=1)
+        fig.update_layout(self.get_layout())
+        fig.show()
