8ac19e1cbfb2588f4044b15c6145b6620aeb14d2
k1n4kut4 <recruitlukejones@gmail.com>
2024-04-24 02:20:45 +0100

    Trailing SMA

--- backtesting_microservice/src/services/strategy_service/sma.py
+++ backtesting_microservice/src/services/strategy_service/sma.py
@@ -10,7 +10,7 @@ import yfinance as yfin
 yfin.pdr_override()
 # https://kernc.github.io/backtesting.py/doc/backtesting/#gsc.tab=0
 from backtesting import Strategy
-from backtesting.lib import crossover
+from backtesting.lib import crossover, SignalStrategy, TrailingStrategy
 from backtesting.test import SMA
 import pandas as pd
 
@@ -50,6 +50,79 @@ class SmaCross(Strategy):
         #       self.sma1[-1] < self.sma2[-1]):
         #     self.position.close()
         #     self.sell()
+
+class SmaCross__Trailing(SignalStrategy, TrailingStrategy):
+    n1 = 10
+    n2 = 25
+    
+    def init(self):
+        # In init() and in next() it is important to call the
+        # super method to properly initialize the parent classes
+        super().init()
+        
+        # Precompute the two moving averages
+        sma1 = self.I(SMA, self.data.Close, self.n1)
+        sma2 = self.I(SMA, self.data.Close, self.n2)
+        
+        # Where sma1 crosses sma2 upwards. Diff gives us [-1,0, *1*]
+        signal = (pd.Series(sma1) > sma2).astype(int).diff().fillna(0)
+        signal = signal.replace(-1, 0)  # Upwards/long only
+        
+        # Use 95% of available liquidity (at the time) on each order.
+        # (Leaving a value of 1. would instead buy a single share.)
+        entry_size = signal * .95
+                
+        # Set order entry sizes using the method provided by 
+        # `SignalStrategy`. See the docs.
+        self.set_signal(entry_size=entry_size)
+        
+        # Set trailing stop-loss to 2x ATR using
+        # the method provided by `TrailingStrategy`
+        self.set_trailing_sl(2)
+
+class Sma4Cross(Strategy):
+    n1 = 50
+    n2 = 100
+    n_enter = 20
+    n_exit = 10
+    
+    def init(self):
+        self.sma1 = self.I(SMA, self.data.Close, self.n1)
+        self.sma2 = self.I(SMA, self.data.Close, self.n2)
+        self.sma_enter = self.I(SMA, self.data.Close, self.n_enter)
+        self.sma_exit = self.I(SMA, self.data.Close, self.n_exit)
+        
+    def next(self):
+        
+        if not self.position:
+            
+            # On upwards trend, if price closes above
+            # "entry" MA, go long
+            
+            # Here, even though the operands are arrays, this
+            # works by implicitly comparing the two last values
+            if self.sma1 > self.sma2:
+                if crossover(self.data.Close, self.sma_enter):
+                    self.buy()
+                    
+            # On downwards trend, if price closes below
+            # "entry" MA, go short
+            
+            else:
+                if crossover(self.sma_enter, self.data.Close):
+                    self.sell()
+        
+        # But if we already hold a position and the price
+        # closes back below (above) "exit" MA, close the position
+        
+        else:
+            if (self.position.is_long and
+                crossover(self.sma_exit, self.data.Close)
+                or
+                self.position.is_short and
+                crossover(self.data.Close, self.sma_exit)):
+                
+                self.position.close()
 
 def calculate_sma__days(df: pd.DataFrame, days):
     return ta.sma(df['Close'], int(days))
